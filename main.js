24fe25889341b4937e13b620d4318f9bb94b085d
Ana Tarrisse <tarrisse@work.co>
2020-02-18 15:34:44 -0300

    feat(global): package main and details

---
+++ main.js
@@ -0,0 +1,5 @@
+const csvToObject = require("./src/functions/csv-to-object/csv-to-object");
+const objectToCss = require("./src/functions/object-to-css/object-to-css");
+
+exports.csvToObject = csvToObject;
+exports.objectToCss = objectToCss;
